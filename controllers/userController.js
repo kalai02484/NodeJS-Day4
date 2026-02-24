60b875b3966fb6e0829471178250027b831aefce
kalai02484 <kalai02484@gmail.com>
2026-02-25 00:49:48 +0530

    Register done with bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userSchema.js";
+import bcrypt from "bcrypt";
 
 // Controller function to handle user registration
 export const registerUser = async (req, res) => {
@@ -9,10 +10,14 @@ export const registerUser = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-    // Create a new user
-    const newUser = new User({ username, email, password });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+
     await newUser.save();
-    res.status(201).json({ message: "User registered successfully", user: newUser });
+    res
+      .status(200)
+      .json({ message: "User registered successfully", user: newUser });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
