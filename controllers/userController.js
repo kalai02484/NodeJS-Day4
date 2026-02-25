3080f2408c0e7d2bfae2de0786ac86bafebae62d
kalai02484 <kalai02484@gmail.com>
2026-02-25 12:14:16 +0530

    Login part done with bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,22 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+// Controller functions for login
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email, User not Found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
